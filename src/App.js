63334d4d4c5b528bec86b4ca950fa034ea3dc781
Nikesh Biraggari <nikeshbiraggari@Nikeshs-MacBook-Air.local>
2024-09-24 18:40:07 -0400

    ProjectSetup

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Switch } from '@mui/material';
+import { Dashboard } from './components/Dashboard';
+import {StockDetails} from './components/StockDetails'
+import {Portfolio} from './components/Portfolio'
+import {WatchList} from './components/Watchlist'
+
+function App() {
+  return(
+    <Router>
+      <div className='app'>
+        <Header/>
+        <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/stocks/:symbol" component={StockDetails}/>
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/watchlist" component={WatchList}/>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App;
