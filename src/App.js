9fd06933f3ad0fc95bb7a4d89b9e948a9ea78a88
Nikesh Biraggari <biraggari.n@northeastern.edu>
2024-09-24 19:33:20 -0400

    CI, Components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import { Router } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Switch } from '@mui/material';
-import { Dashboard } from './components/Dashboard';
-import {StockDetails} from './components/StockDetails'
-import {Portfolio} from './components/Portfolio'
-import {WatchList} from './components/Watchlist'
+import Dashboard from './components/Dashboard';
+import StockDetails from './components/StockDetails'
+import Portfolio from './components/Portfolio'
+import WatchList from './components/Watchlist'
+import Header from './components/Header';
 
 function App() {
   return(
     <Router>
-      <div className='app'>
+      <div className="app">
         <Header/>
         <Switch>
             <Route exact path="/" component={Dashboard} />
---
+++ src/components/Watchlist.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function Watchlist(){
+
+}
+
+export default Watchlist;
